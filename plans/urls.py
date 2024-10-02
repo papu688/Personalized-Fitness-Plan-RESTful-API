6b759db1f1088959cfcf8269f6ffdebc27718fa7
papu_68 <giorgobianipaul@gmail.com>
2024-10-02 16:28:12 +0200

    fixed bugs

--- plans/urls.py
+++ plans/urls.py
@@ -1,6 +1,27 @@
-from django.urls import path
-from rest_framework.routers import DefaultRouter
-from views import *
 
-router = DefaultRouter()
-router.register(r'users', views.User)
+from django.urls import path
+from .views import (
+    WorkoutListCreateAPIView,
+    WorkoutDetailAPIView,
+    WorkoutPlanListCreateAPIView,
+    WorkoutPlanDetailAPIView,
+    NutritionPlanListCreateAPIView,
+    NutritionPlanDetailAPIView,
+    WorkoutProgressListCreateAPIView,
+    WorkoutProgressDetailAPIView,
+)
+
+urlpatterns = [
+    path('workouts/', WorkoutListCreateAPIView.as_view(), name='workout-list-create'),
+    path('workouts/<int:pk>/', WorkoutDetailAPIView.as_view(), name='workout-detail'),
+    
+    path('workout-plans/', WorkoutPlanListCreateAPIView.as_view(), name='workout-plan-list-create'),
+    path('workout-plans/<int:pk>/', WorkoutPlanDetailAPIView.as_view(), name='workout-plan-detail'),
+    
+    path('nutrition-plans/', NutritionPlanListCreateAPIView.as_view(), name='nutrition-plan-list-create'),
+    path('nutrition-plans/<int:pk>/', NutritionPlanDetailAPIView.as_view(), name='nutrition-plan-detail'),
+    
+    path('workout-progress/', WorkoutProgressListCreateAPIView.as_view(), name='workout-progress-list-create'),
+    path('workout-progress/<int:pk>/', WorkoutProgressDetailAPIView.as_view(), name='workout-progress-detail'),
+]
+
