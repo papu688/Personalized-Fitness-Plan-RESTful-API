c59d3176a9528fdb83806e6bdd5e80300e036644
papu_68 <giorgobianipaul@gmail.com>
2024-10-02 11:50:52 +0200

    Add CustomUserDetailsSerializer for user creation and updates

--- fitness_API/urls.py
+++ fitness_API/urls.py
@@ -4,10 +4,11 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('plans.urls'))
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- plans/serializers.py
+++ plans/serializers.py
@@ -5,4 +5,29 @@ class CustomUserDetailsSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'email', 'name', 'password', 'is_admin', 'created_at', 'updated_at']
-        read_only_fields = ['is_admin', 'created_at', 'updated_at']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+        def create(self, validated_data):
+            user = User(
+                email=validated_data['email'],
+                name=validated_data['name'],
+                is_admin=validated_data.get('is_admin', False)
+            )
+            user.set_password(validated_data['password'])
+            user.save()
+            return user
+        
+        def update(self, instance, validated_data):
+            instance.email = validated_data('email', instance.email)
+            instance.name = validated_data.get('name', instance.name)
+            instance.is_active = validated_data.get('is_active', instance.is_active)
+            instance.is_admin = validated_data.get('is_admin', instance.is_admin)
+
+            password = validated_data.get('password', None)
+            if password:
+                instance.set_password(password)
+
+            instance.save()
+            return instance
